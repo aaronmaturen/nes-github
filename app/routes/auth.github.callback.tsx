b374f208e68ab4af5ef64cc6bd8becc05540fa35
Aaron Maturen <aaron.maturen@gmail.com>
2023-08-06 16:59:26 -0400

    fix /dashboard

--- app/routes/auth.github.callback.tsx
+++ app/routes/auth.github.callback.tsx
@@ -3,7 +3,7 @@ import { authenticator } from "~/services/auth.server";
 
 export async function loader({ request }: LoaderArgs) {
   return authenticator.authenticate("github", request, {
-    successRedirect: "/dashboard",
+    successRedirect: "/",
     failureRedirect: "/login",
     throwOnError: true,
   });
