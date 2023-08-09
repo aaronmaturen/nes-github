fc49b234e567dac1aea9740856279fd082a415a6
Aaron Maturen <aaron.maturen@gmail.com>
2023-08-09 10:50:29 -0400

    more defensive with github

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -2,8 +2,11 @@
 import { renderers, type RenderableTreeNodes } from "@markdoc/markdoc";
 import * as React from "react";
 
-type Props = { content: RenderableTreeNodes };
+type Props = { content: RenderableTreeNodes | string };
 
 export function Markdown({ content }: Props) {
+  if (typeof content === "string") {
+    return <pre>{content}</pre>;
+  }
   return <>{renderers.react(content, React)}</>;
 }
--- app/services/markdown.server.ts
+++ app/services/markdown.server.ts
@@ -1,6 +1,10 @@
 // app/services/markdown.server.ts
 import { parse, transform, type RenderableTreeNodes } from "@markdoc/markdoc";
 
-export function parseMarkdown(markdown: string): RenderableTreeNodes {
-  return transform(parse(markdown));
+export function parseMarkdown(markdown: string): RenderableTreeNodes | string {
+  try {
+    return transform(parse(markdown));
+  } catch (error) {
+    return markdown;
+  }
 }
